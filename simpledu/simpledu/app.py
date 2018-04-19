162bd87ea0ca3a84a8dd38df33fe3ee98d13d717
Sophie521 <18782249841@163.com>
2018-04-19 16:01:38 +0800

    database and forms

--- simpledu/simpledu/app.py
+++ simpledu/simpledu/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 from simpledu.config import configs
 from simpledu.models import db, Course
+from flask_migrate import Migrate
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(configs.get(config))
     db.init_app(app)
+    Migrate(app,db)
     register_blueprints(app)
     return app
 
