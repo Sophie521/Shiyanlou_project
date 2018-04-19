f78890b5b867a9b843aa56188381f3fe38948085
Sophie521 <18782249841@163.com>
2018-04-19 16:12:01 +0800

    edu db forms on branch

---
+++ edu_db_and_forms/simpledu/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import Length, Email, EqualTo, Required
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('username', validators=[Required(), Length(3, 24)])
+    email = StringField('email', validators=[Required(), Email()])
+    password = PasswordField('password', validators=[Required(), Length(6, 24)])
+    repeat_password = PasswordField('password again', validators=[Required(), EqualTo('password')])
+    submit = SubmitField('submit')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[Required(), Email()])
+    password = PasswordField('password', validators=[Required(), Length(6, 24)])
+    remember_me = BooleanField('password again')
+    submit = SubmitField('submit')
+
